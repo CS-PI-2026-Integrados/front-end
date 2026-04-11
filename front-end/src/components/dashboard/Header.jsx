f50b85f430bae56694e798426a287408f61774f4
Luan Gabriel <38366327+luan004@users.noreply.github.com>
2026-04-10 22:01:27 -0300

    feat: layout básico do dashboard

--- front-end/src/components/dashboard/Header.jsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@/components/ui/button";
-
-export default function Header() {
-  return (
-    <>
-      <Button>Testando</Button>
-    </>
-  );
-}
--- front-end/src/layout/DashboardLayout.jsx
+++ front-end/src/layout/DashboardLayout.jsx
@@ -1,13 +1,68 @@
+import { ChartBar, FileText, FolderArchive, LayoutDashboard, Settings, Users } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { Outlet } from "react-router-dom";
-import Header from "../components/dashboard/Header";
+
 
 export default function DashboardLayout() {
   return (
     <>
-      <Header />
-      <main>
-        <Outlet />
-      </main>
+      <div className="h-dvh flex flex-col">
+        <header className="h-14 border-b flex items-center justify-between bg-background">
+          <div className="w-60 h-full flex px-4 border-e">
+            <span className="my-auto text-green-700 font-medium">SICAPE</span>
+          </div>
+          <div className="flex items-center gap-3 px-4">
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="ghost" className="p-0 h-auto">
+                  <Avatar>
+                    <AvatarFallback>LG</AvatarFallback>
+                  </Avatar>
+                </Button>
+              </DropdownMenuTrigger>
+
+              <DropdownMenuContent align="end">
+                <DropdownMenuItem>Perfil</DropdownMenuItem>
+                <DropdownMenuItem>Sair</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </header>
+        <div className="flex h-full">
+          <aside className="w-60 p-4 border-e h-full flex flex-col gap-1">
+            <Button className="justify-start">
+              <LayoutDashboard /> Dashboard
+            </Button>
+            <Button variant="ghost" className="justify-start">
+              <Users /> Apenados
+            </Button>
+            <Button variant="ghost" className="justify-start">
+              <FileText /> Comprovantes
+            </Button>
+            {/* <Button variant="ghost" className="justify-start">
+              <FolderArchive /> Documentos
+            </Button>
+            <Button variant="ghost" className="justify-start">
+              <ChartBar /> Relatorios
+            </Button> */}
+            <Button variant="ghost" className="justify-start">
+              <Settings /> Configuracões
+            </Button>
+          </aside>
+          <main className="p-4">
+            <Outlet />
+          </main>
+        </div>
+      </div>
+      
     </>
   );
 }
