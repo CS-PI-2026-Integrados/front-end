240784f176ea8558d2a288d6a63e909ec9b4244c
diegomatheuscg <diegogon699@gmail.com>
2026-05-02 21:41:08 -0300

    feat: adiciona card de cadastro de atendimento

--- src/components/service/ConvictedCard.jsx
+++ src/components/service/ConvictedCard.jsx
@@ -1,6 +1,17 @@
 import { cn } from '@/lib/utils'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { SelectConvicted } from './SelectConvicted.jsx'
 
-const ConvictedCard = () => {
-  return <Card></Card>
+export function ConvictedCard({ className }) {
+  return (
+    <Card className={cn('shadow-sm', className)}>
+      <CardHeader className="flex-column items-center space-y-0 pb-2">
+        <CardTitle className="font-bold">Dados do atendimento</CardTitle>
+        <p className="text-muted-foreground">Selecione o apenado e registre a foto</p>
+      </CardHeader>
+      <CardContent className="flex-column w-full items-center">
+        <SelectConvicted></SelectConvicted>
+      </CardContent>
+    </Card>
+  )
 }
