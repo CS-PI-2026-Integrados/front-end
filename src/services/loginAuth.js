4570ae55d775333d6fc4b96075ec73948c52a890
Brayan Barros <brayanbarrosdm@gmail.com>
2026-05-06 20:22:51 -0300

    fix: corrigindo rota do dashboard

--- src/services/loginAuth.js
+++ src/services/loginAuth.js
@@ -1,24 +1,19 @@
-import mock_users from '@/mocks/mock-users.json'
+import { mockUsers } from '@/mocks/users.mock'
 
 export const authenticateUser = async (cpf, password) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      const userFound = mock_users.find((user) => user.cpf === cpf && user.password === password)
+      const userFound = mockUsers.users.find(
+        (user) => user.cpf === cpf && user.password === password
+      )
 
       if (userFound) {
         const fakeToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.mockToken_' + Date.now()
         // eslint-disable-next-line no-unused-vars
-        const { password: _, tenant: originalTenant, ...userData } = userFound
-
-        const formattedTenant = originalTenant
-          .toLowerCase()
-          .replace(/\s+/g, '')
-          .normalize('NFD')
-          .replace(/[\u0300-\u036f]/g, '')
+        const { password: _, ...userData } = userFound
 
         resolve({
           user: userData,
-          tenant: formattedTenant,
           token: fakeToken,
         })
       } else {
